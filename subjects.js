90af71a1b093ef489fe06e5010e61a2030bc2e79
BoluwatifeOlufolahan <73609662+BoluwatifeOlufolahan@users.noreply.github.com>
2020-11-18 15:53:00 +0100

    Add files via upload

---
+++ subjects.js
@@ -0,0 +1,82 @@
+let subjects = [
+    {
+        id: 1,
+        title: "Maths",
+        location: "London",
+        price: 10000,
+        image: "images/calc1.png",
+        spaces: 5
+    },
+    {
+        id: 2,
+        title: "Further Maths",
+        location: "Oxford",
+        price: 10000,
+        image: "images/calc1.png",
+        spaces: 5
+    },
+    {
+        id: 3,
+        title: "English",
+        location: "Nigeria",
+        price: 10000,
+        image: "images/calc1.png",
+        spaces: 5
+    },
+    {
+        id: 4,
+        title: "Literature",
+        location: "York",
+        price: 8000,
+        image: "images/calc1.png",
+        spaces: 5
+    },
+    {
+        id: 5,
+        title: "Music",
+        location: "Bristol",
+        price: 9000,
+        image: "images/calc1.png",
+        spaces: 5
+    },
+    {
+        id: 6,
+        title: "Biology",
+        location: "London",
+        price: 10000,
+        image: "images/calc1.png",
+        spaces: 5
+    },
+    {
+        id: 7,
+        title: "Chemistry",
+        location: "Oxford",
+        price: 10000,
+        image: "images/calc1.png",
+        spaces: 5
+    },
+    {
+        id: 8,
+        title: "French",
+        location: "London",
+        price: 10000,
+        image: "images/calc1.png",
+        spaces: 5
+    },
+    {
+        id: 9,
+        title: "Civic Education",
+        location: "York",
+        price: 8000,
+        image: "images/calc1.png",
+        spaces: 5
+    },
+    {
+        id: 10,
+        title: "Economics",
+        location: "Bristol",
+        price: 9000,
+        image: "images/calc1.png",
+        spaces: 5
+    }
+]
